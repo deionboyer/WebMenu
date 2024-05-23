c1f962baadb8cb0e2fc8e12e6f7905e9dd632c7c
Deion Boyer <boyerdeion@gmail.com>
2024-05-22 21:11:05 -0400

    Created ImageUrl Property, Created ImageUrl column in SSMS Styled my Index for Items and Home

--- MenuItems.DataAccess.EF/Models/Items.cs
+++ MenuItems.DataAccess.EF/Models/Items.cs
@@ -13,6 +13,7 @@ namespace MenuItems.DataAccess.EF.Models
         public string ItemName { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
         public Items() { }
         public Items(int itemID, string itemName, string mealType ,string description, decimal price)
         {
